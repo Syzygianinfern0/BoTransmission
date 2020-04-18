8f7b5ba8b7744c9bc9956792c402136306c6f95d
Syzygianinfern0 <spsharan2000@gmail.com>
2020-04-18 14:32:00 +0530

    Heroku update

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python botransmission.py
--- botransmission.py
+++ botransmission.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient, events
 from configs import *
 
 with TelegramClient('BoTransmission', API_ID, API_HASH) as client:
-    client.send_message('me', 'Telethon Is Up!  ')
+    client.send_message('me', 'Telethon Is Up!')
 
 
     @client.on(events.NewMessage(chats=triggerx_chat_id, from_users=delta_leechx_id))
