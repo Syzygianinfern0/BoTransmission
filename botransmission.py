906a21f806451cb42f92d0b25f099ef774bc0692
Syzygianinfern0 <spsharan2000@gmail.com>
2020-04-18 14:28:06 +0530

    Initial Push

---
+++ botransmission.py
@@ -0,0 +1,14 @@
+from telethon.sync import TelegramClient, events
+
+from configs import *
+
+with TelegramClient('BoTransmission', API_ID, API_HASH) as client:
+    client.send_message('me', 'Telethon Is Up!  ')
+
+
+    @client.on(events.NewMessage(chats=triggerx_chat_id, from_users=delta_leechx_id))
+    async def handler(event):
+        await event.message.forward_to(leechx_chat_id)
+
+
+    client.run_until_disconnected()
